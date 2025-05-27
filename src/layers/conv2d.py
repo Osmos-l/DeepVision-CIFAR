cc8d3646654d408251f1cc75abda2b609198adad
Jordan LECLERCQ <emailjeux81@gmail.com>
2025-05-27 20:49:22 +0200

    feat(layer/conv2d): backward

--- src/layers/conv2d.py
+++ src/layers/conv2d.py
@@ -52,3 +52,58 @@ class Conv2D:
                         out[image_index, i, j, oc] = np.sum(patch * self.weights[oc]) + self.bias[oc]
 
         return out
+
+    def backward(self, dout):
+        batch_size, h_in, w_in, in_channels = self.x.shape
+        _, h_out, w_out, out_channels = dout.shape
+        
+        # Initialize gradients with zeros
+        dx = np.zeros_like(self.x)
+        dW = np.zeros_like(self.weights)
+        db = np.zeros_like(self.bias)
+        
+        # Pad input and gradient w.r.t input for padding
+        x_padded = np.pad(self.x,
+                          ((0,0), (self.padding, self.padding), (self.padding, self.padding), (0,0)),
+                          mode='constant')
+        dx_padded = np.pad(dx,
+                           ((0,0), (self.padding, self.padding), (self.padding, self.padding), (0,0)),
+                           mode='constant')
+        
+        # Compute gradient w.r.t bias: sum over batch and spatial dimensions
+        db = np.sum(dout, axis=(0,1,2))
+        
+        # Compute gradients for weights and input
+        for image_index in range(batch_size):
+            for i in range(h_out):
+                for j in range(w_out):
+                    for oc in range(out_channels):
+                        h_start = i * self.stride
+                        w_start = j * self.stride
+                        
+                        # Extract the patch from padded input
+                        patch = x_padded[image_index,
+                                        h_start:(h_start + self.kernel_size), 
+                                        w_start:(w_start + self.kernel_size), 
+                                        :]
+                        
+                        # Update gradient w.r.t weights: multiply patch by dout scalar at this position
+                        dW[oc] += patch * dout[image_index, i, j, oc]
+                        
+                        # Update gradient w.r.t input (padded): multiply weights by dout scalar
+                        dx_padded[image_index, 
+                                h_start:(h_start + self.kernel_size), 
+                                w_start:(w_start + self.kernel_size), 
+                                :] += self.weights[oc] * dout[image_index, i, j, oc]
+        
+        # Remove padding from dx_padded to get dx with original input shape
+        if self.padding > 0:
+            dx = dx_padded[:, self.padding:-self.padding, self.padding:-self.padding, :]
+        else:
+            dx = dx_padded
+        
+        # Save gradients for optimizer update
+        self.dW = dW
+        self.db = db
+        
+        return dx
