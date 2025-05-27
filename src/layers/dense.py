a8818833480855758a5fd749bb611711c2b3a4c1
Jordan LECLERCQ <emailjeux81@gmail.com>
2025-05-27 20:28:37 +0200

    feat(layer/dense): backward

--- src/layers/dense.py
+++ src/layers/dense.py
@@ -7,3 +7,15 @@ class Dense:
 
     def forward(self, x):
         return np.dot(x, self.weights) + self.bias
+
+    def backward(self, dout):
+        # Gradients
+        self.dW = np.dot(self.input.T, dout)    # [input_dim, batch] x [batch, output_dim] -> [input_dim, output_dim]
+        self.db = np.sum(dout, axis=0)          # [batch, output_dim] -> [output_dim]
+        dx = np.dot(dout, self.weights.T)       # [batch, output_dim] x [output_dim, input_dim] -> [batch, input_dim]
+
+        return dx # Gradient for next layer
+
+    def update(self, learning_rate):
+        self.weights -= learning_rate * self.dW
+        self.bias -= learning_rate * self.db
